1eab5ec5358c7f43b15325697b01fb70b52b8f65
Olha Andriiets <osandriets@gmail.com>
2024-12-23 20:20:03 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+  ],
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'my-angular-app';
+}
